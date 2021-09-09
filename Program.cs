0212f3c87cf1105204b01bad7eb8ddaaa4fdc27b
Bruno-uk <75496684+Bruno-uk@users.noreply.github.com>
2021-09-09 19:05:10 +0300

    Lb
    lb

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo
+            {
+                NumberDecimalSeparator = ".",
+            };
+            
+            double v1, v2,a,s;
+            Console.WriteLine("Write v1");
+            str= Console.ReadLine();
+            v1=Convert.ToDouble(str, numberFormatInfo);
+
+            Console.WriteLine("Write v2");
+            str = Console.ReadLine();
+            v2 = Convert.ToDouble(str, numberFormatInfo);
+
+            Console.WriteLine("Write a");
+            str = Console.ReadLine();
+            a = Convert.ToDouble(str, numberFormatInfo);
+
+            Console.WriteLine("Write s");
+            str = Console.ReadLine();
+            s = Convert.ToDouble(str, numberFormatInfo);
+            
+            
+            double res = (s*a)/(v1 + v2);
+            Console.WriteLine(res);
+
+            Console.ReadLine();
+        }
+    }
+}
